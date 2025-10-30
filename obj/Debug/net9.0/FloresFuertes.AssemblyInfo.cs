2a793d0fdabd60d4a728c6828a54e37012c7c8e5
Anson <23106549@student.hhs.nl>
2025-10-30 11:38:19 +0100

    Changed Login Pagina met styling
    heb wat aanpassingen gemaakt van login met wat styling

--- obj/Debug/net9.0/FloresFuertes.AssemblyInfo.cs
+++ obj/Debug/net9.0/FloresFuertes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FloresFuertes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079c27cb2a425530d99f43f545378609ce3e2850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98fbf9f138fd617dcdb37f0116efbd7aa5e59b98")]
 [assembly: System.Reflection.AssemblyProductAttribute("FloresFuertes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FloresFuertes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
