54e1404fca1f3a9e0a556b9683ef5dcfa66a10d8
Anson <23106549@student.hhs.nl>
2025-10-30 12:27:07 +0100

    Fixed Authcontext

--- frontend/app/context/AuthContext.js
+++ frontend/app/context/AuthContext.js
@@ -1,18 +1,28 @@
 "use client"
 
-import {createContext, useContext, useState} from 'react';
+import {createContext, useContext, useState, useEffect, use} from 'react';
 
 const  AuthContext = createContext();
+
 export function AuthProvider( {children} ) {
     const [user, setUser] = useState(null);
     const isLoggedIn = !!user;
 
+    useEffect(() => {
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
     const  login = (userData) => {
             setUser(userData);
+            localStorage.setItem("user", JSON.stringify(userData));
         };
 
         const logout = () => {
             setUser(null);
+            localStorage.removeItem("user");
         };
     return (
         <AuthContext.Provider value={{user, isLoggedIn, login, logout}}>
