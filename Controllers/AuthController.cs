a25a4437ed47570fae290496ff094cf9f70a84a3
dami <dskastaneer@gmail.com>
2025-11-17 11:04:04 +0100

    Renovatie
    - Fixed everything

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -63,7 +63,17 @@ namespace FloresFuertes.Controllers
 
             await _context.SaveChangesAsync();
 
-            return Ok(gebruiker);
+            return Ok(new
+            {
+                gebruiker_Id = gebruiker.Gebruiker_Id,
+                voornaam = gebruiker.Voornaam,
+                achternaam = gebruiker.Achternaam,
+                email = gebruiker.Email,
+                adres = gebruiker.Adres,
+                telefoonnr = gebruiker.Telefoonnr,
+                woonplaats = gebruiker.Woonplaats,
+                gebruikerType = gebruiker.GebruikerType
+            });
         }
     }
 }
