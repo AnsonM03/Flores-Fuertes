c93d7161efa653ef06367ec46fceb98dc3e6d13d
Anson <23106549@student.hhs.nl>
2025-12-11 11:10:08 +0100

    Changed biedingen

--- frontend/app/biedingen/[id]/page.js
+++ frontend/app/biedingen/[id]/page.js
@@ -1,10 +1,10 @@
-"use client";
+//"use client";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import "../styles/biedingen.css";
+//import { useEffect, useState } from "react";
+//import Link from "next/link";
+//import { useRouter } from "next/navigation";
+//import "../styles/biedingen.css";
 
-export default function BiedingenPage({ veilingId }) {
+//export default function BiedingenPage({ veilingId }) {
     
-}
+//}
